2ac46670519b13bae221dc4796a2e462b15fb1be
mabarril <mabarril@gmail.com>
2026-06-02 20:53:22 -0300

    chore: fix dependencies vulnerabilities, TS config, allowScripts & test suite

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
     production: false,
-    supabaseUrl: import.meta.env.VITE_SUPABASE_URL || '',
-    supabaseKey: import.meta.env.VITE_SUPABASE_ANON_KEY || ''
+    supabaseUrl: import.meta.env['VITE_SUPABASE_URL'] || '',
+    supabaseKey: import.meta.env['VITE_SUPABASE_ANON_KEY'] || ''
 };
